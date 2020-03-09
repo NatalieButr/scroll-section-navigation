3203a82ab66ae4abef4f1695be6d6daa38a0f26f
NatalieButr <natashabutrim@gmail.com>
2020-03-09 18:38:57 +0300

    create Footer component

--- section-navigation/src/App.js
+++ section-navigation/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import './App.css';
-import Characters from './components/Characters';
-import CharactersList from './components/CharactersList';
 import Header from './components/Header/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
+       <Header />
+        <Main />
+        <Footer />
     </div>
   );
 }
