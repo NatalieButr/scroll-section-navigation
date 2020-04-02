06917366588f64e4e80b38c71cbf21bcd8414268
NatalieButr <natashabutrim@gmail.com>
2020-04-02 21:18:17 +0300

    safe http

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // get
 export const getCharacters = () => dispatch => {
-  axios('http://hp-api.herokuapp.com/api/characters/students')
+  axios('https://hp-api.herokuapp.com/api/characters/students')
     .then(({ data }) => dispatch(getCharactersSuccess(data)))
     .catch(error => dispatch(getCharactersError(error)));
 }
