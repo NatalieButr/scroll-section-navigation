96c4e604054d33ec5b2bf44759ab3433203b0855
NatalieButr <natashabutrim@gmail.com>
2020-04-03 11:36:24 +0300

    conditional rendering in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,13 +9,11 @@ import Characters from '../Characters'
 const Main = () => {
 
     const dispatch = useDispatch()
-    const characters = useSelector(state =>state.characters.characters)
+    const characters = useSelector(state => state.characters.characters)
     const refs = characters.reduce((refsObj, character) => {
         refsObj[character.name] = createRef();
         return refsObj;
       }, {});
-
-      
 
       const handleClick = (name) => {
           dispatch({ type: 'SET_ACTIVE_CHARACTER', payload: {name: name, click: true} })
@@ -24,10 +22,19 @@ const Main = () => {
           block: 'center',
         });
       };
+
+     console.log(characters)
+
+
     return (
         <main>
-            <CharactersList handleClick={handleClick} characters={characters}/>
-            <Characters refs={refs} characters={characters} />
+        {characters.length === 0 ?
+         'Идет загрузка' :
+          <>
+          <CharactersList handleClick={handleClick} characters={characters}/>
+          <Characters refs={refs} characters={characters} />
+          </>
+        }
         </main>
     )
 }
